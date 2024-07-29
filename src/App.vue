1cae67da90f58e41b24c5d768d1297fe1b9d54ad
spl-RerreRojas <renata.rojas07@gmail.com>
2024-07-29 19:21:58 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <AddUsers/>
+  <NewUsers/>
+
+</template>
+
+<script>
+import AddUsers from './components/AddUsers.vue';
+import NewUsers from './components/NewUsers.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AddUsers,
+    NewUsers
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/firebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA8dxQEaw2BPjF-s3rS3RtA8AQH2h6lUrQ",
+  authDomain: "crud-firebase-d5e44.firebaseapp.com",
+  projectId: "crud-firebase-d5e44",
+  storageBucket: "crud-firebase-d5e44.appspot.com",
+  messagingSenderId: "914143097240",
+  appId: "1:914143097240:web:7610b9727deeeed1d43bed"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app; 
